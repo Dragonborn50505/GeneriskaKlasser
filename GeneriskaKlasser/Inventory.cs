d6827e8db0483f162930d56f12d3870de6808f2a
Vincent Allinggård <allinggrd.vincent@elev.ga.ntig.se>
2023-02-09 11:06:25 +0100

    first commit

---
+++ GeneriskaKlasser/Inventory.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+public class Inventory
+{
+    int HealingPotions = 0;
+    int MagicPotions = 0;
+    int StaminaPotions = 0;
+
+}
---
+++ GeneriskaKlasser/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Numerics;
+using System.Collections.Generic;
+// using Raylib_cs;
+
+// Queue<string> ppl = new Queue<string>();
+
+// ppl.Enqueue("Micke");
+// ppl.Enqueue("Matin");
+// ppl.Enqueue("Lena");
+
+// // string p = ppl.Dequeue();
+// string p = ppl.Peek();
+
+// Console.WriteLine(p);
+
+// Console.ReadLine();
+
+
+
+
+
+
+
+
+// Queue<int> slumptal = new Queue<int>();
+// Random generator = new Random();
+
+// for (int i = 0; i < 10; i++)
+// {
+//     int t = generator.Next(100);
+//     slumptal.Enqueue(t);
+// }
+
+// while (slumptal.Count > 0)
+// {
+//     Console.WriteLine(slumptal.Dequeue());
+// }
+// Console.ReadLine();
+
+
+
+
+Dictionary<string, string> words = new();
+words.Add ("Hallo", "Guten tag");
+Console.WriteLine("Hallo");
+
+Console.ReadLine();
+
+
+
+
+
+
+
+
+// Queue<string> potionBrewing = new Queue<string>();
+
+// ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+
+// if (keyInfo.Key == ConsoleKey.D1)
+// {
+//     potionBrewing.Enqueue("Healing Potion");
+
+// }
+// if (keyInfo.Key == ConsoleKey.D2)
+// {
+//     potionBrewing.Enqueue("Magic Potion");
+
+// }
+// if (keyInfo.Key == ConsoleKey.D3)
+// {
+//     potionBrewing.Enqueue("Stamina Potion");
+// }
+//  Console.ReadLine();
+
+//  string o = potionBrewing.Peek();
+//  Console.WriteLine(o);
+// //  Console.WriteLine(HealingPotions);
+
+//  Console.ReadLine();
